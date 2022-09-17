7a8fefabb2410cf5f9680d8100b8f3b60953adc0
Eddie Gao <egao1@ocdsb.ca>
2022-09-16 20:18:37 -0400

    Update settings.py

--- Portfolio/mywebsite/mywebsite/settings.py
+++ Portfolio/mywebsite/mywebsite/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,8 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
